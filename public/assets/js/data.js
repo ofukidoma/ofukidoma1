fc80cd421509adac1b2f5c93a0d7c37221be7bdd
joelodufu <joel.odufu@gmail.com>
2022-02-08 01:44:57 +0100

    commited succesfully

---
+++ public/assets/js/data.js
@@ -0,0 +1,72 @@
+export const members =[
+    {
+        name:"Rtd. Comptroller Sylvester Ode Okpe",
+        position:"Treasurer of Ofuk’Idoma Development Association",
+        portfolio:"He attended St. Barnard’s Primary School, Okpoga, Mt. St. Michael’s Secondary School, Aliade and University of Calabar, in Cross River State where he had his Bachelor of Arts and Master of Arts Degrees",
+        img:"https://firebasestorage.googleapis.com/v0/b/ofukidoma1-8a9d7.appspot.com/o/TEAM%2Foche.jpg?alt=media&token=095d1099-d8f0-462e-acf0-b8586c83cc47",
+        about:"having a successful career with the Nigeria Customs Service as a retired Comptroller of Customs, he developed some management and leadership skills especially the power of effective supervision and perfection in the performance of assigned duties. And he is happily married to Mrs Patricia Ene Okpe."
+    },
+    {
+        name:"Rtd. Comptroller Sylvester Ode Okpe",
+        position:"Treasurer of Ofuk’Idoma Development Association",
+        portfolio:"He attended St. Barnard’s Primary School, Okpoga, Mt. St. Michael’s Secondary School, Aliade and University of Calabar, in Cross River State where he had his Bachelor of Arts and Master of Arts Degrees",
+        img:"https://firebasestorage.googleapis.com/v0/b/ofukidoma1-8a9d7.appspot.com/o/TEAM%2Foche.jpg?alt=media&token=095d1099-d8f0-462e-acf0-b8586c83cc47",
+        about:"having a successful career with the Nigeria Customs Service as a retired Comptroller of Customs, he developed some management and leadership skills especially the power of effective supervision and perfection in the performance of assigned duties. And he is happily married to Mrs Patricia Ene Okpe."
+    },
+    {
+        name:"Rtd. Comptroller Sylvester Ode Okpe",
+        position:"Treasurer of Ofuk’Idoma Development Association",
+        portfolio:"He attended St. Barnard’s Primary School, Okpoga, Mt. St. Michael’s Secondary School, Aliade and University of Calabar, in Cross River State where he had his Bachelor of Arts and Master of Arts Degrees",
+        img:"https://firebasestorage.googleapis.com/v0/b/ofukidoma1-8a9d7.appspot.com/o/TEAM%2Foche.jpg?alt=media&token=095d1099-d8f0-462e-acf0-b8586c83cc47",
+        about:"having a successful career with the Nigeria Customs Service as a retired Comptroller of Customs, he developed some management and leadership skills especially the power of effective supervision and perfection in the performance of assigned duties. And he is happily married to Mrs Patricia Ene Okpe."
+    },
+    {
+        name:"Rtd. Comptroller Sylvester Ode Okpe",
+        position:"Treasurer of Ofuk’Idoma Development Association",
+        portfolio:"He attended St. Barnard’s Primary School, Okpoga, Mt. St. Michael’s Secondary School, Aliade and University of Calabar, in Cross River State where he had his Bachelor of Arts and Master of Arts Degrees",
+        img:"https://firebasestorage.googleapis.com/v0/b/ofukidoma1-8a9d7.appspot.com/o/TEAM%2Foche.jpg?alt=media&token=095d1099-d8f0-462e-acf0-b8586c83cc47",
+        about:"having a successful career with the Nigeria Customs Service as a retired Comptroller of Customs, he developed some management and leadership skills especially the power of effective supervision and perfection in the performance of assigned duties. And he is happily married to Mrs Patricia Ene Okpe."
+    },
+    {
+        name:"Rtd. Comptroller Sylvester Ode Okpe",
+        position:"Treasurer of Ofuk’Idoma Development Association",
+        portfolio:"He attended St. Barnard’s Primary School, Okpoga, Mt. St. Michael’s Secondary School, Aliade and University of Calabar, in Cross River State where he had his Bachelor of Arts and Master of Arts Degrees",
+        img:"https://firebasestorage.googleapis.com/v0/b/ofukidoma1-8a9d7.appspot.com/o/TEAM%2Foche.jpg?alt=media&token=095d1099-d8f0-462e-acf0-b8586c83cc47",
+        about:"having a successful career with the Nigeria Customs Service as a retired Comptroller of Customs, he developed some management and leadership skills especially the power of effective supervision and perfection in the performance of assigned duties. And he is happily married to Mrs Patricia Ene Okpe."
+    },
+    {
+        name:"Rtd. Comptroller Sylvester Ode Okpe",
+        position:"Treasurer of Ofuk’Idoma Development Association",
+        portfolio:"He attended St. Barnard’s Primary School, Okpoga, Mt. St. Michael’s Secondary School, Aliade and University of Calabar, in Cross River State where he had his Bachelor of Arts and Master of Arts Degrees",
+        img:"https://firebasestorage.googleapis.com/v0/b/ofukidoma1-8a9d7.appspot.com/o/TEAM%2Foche.jpg?alt=media&token=095d1099-d8f0-462e-acf0-b8586c83cc47",
+        about:"having a successful career with the Nigeria Customs Service as a retired Comptroller of Customs, he developed some management and leadership skills especially the power of effective supervision and perfection in the performance of assigned duties. And he is happily married to Mrs Patricia Ene Okpe."
+    },
+    {
+        name:"Rtd. Comptroller Sylvester Ode Okpe",
+        position:"Treasurer of Ofuk’Idoma Development Association",
+        portfolio:"He attended St. Barnard’s Primary School, Okpoga, Mt. St. Michael’s Secondary School, Aliade and University of Calabar, in Cross River State where he had his Bachelor of Arts and Master of Arts Degrees",
+        img:"https://firebasestorage.googleapis.com/v0/b/ofukidoma1-8a9d7.appspot.com/o/TEAM%2Foche.jpg?alt=media&token=095d1099-d8f0-462e-acf0-b8586c83cc47",
+        about:"having a successful career with the Nigeria Customs Service as a retired Comptroller of Customs, he developed some management and leadership skills especially the power of effective supervision and perfection in the performance of assigned duties. And he is happily married to Mrs Patricia Ene Okpe."
+    },
+    {
+        name:"Rtd. Comptroller Sylvester Ode Okpe",
+        position:"Treasurer of Ofuk’Idoma Development Association",
+        portfolio:"He attended St. Barnard’s Primary School, Okpoga, Mt. St. Michael’s Secondary School, Aliade and University of Calabar, in Cross River State where he had his Bachelor of Arts and Master of Arts Degrees",
+        img:"https://firebasestorage.googleapis.com/v0/b/ofukidoma1-8a9d7.appspot.com/o/TEAM%2Foche.jpg?alt=media&token=095d1099-d8f0-462e-acf0-b8586c83cc47",
+        about:"having a successful career with the Nigeria Customs Service as a retired Comptroller of Customs, he developed some management and leadership skills especially the power of effective supervision and perfection in the performance of assigned duties. And he is happily married to Mrs Patricia Ene Okpe."
+    },
+    {
+        name:"Rtd. Comptroller Sylvester Ode Okpe",
+        position:"Treasurer of Ofuk’Idoma Development Association",
+        portfolio:"He attended St. Barnard’s Primary School, Okpoga, Mt. St. Michael’s Secondary School, Aliade and University of Calabar, in Cross River State where he had his Bachelor of Arts and Master of Arts Degrees",
+        img:"https://firebasestorage.googleapis.com/v0/b/ofukidoma1-8a9d7.appspot.com/o/TEAM%2Foche.jpg?alt=media&token=095d1099-d8f0-462e-acf0-b8586c83cc47",
+        about:"having a successful career with the Nigeria Customs Service as a retired Comptroller of Customs, he developed some management and leadership skills especially the power of effective supervision and perfection in the performance of assigned duties. And he is happily married to Mrs Patricia Ene Okpe."
+    },
+    {
+        name:"Rtd. Comptroller Sylvester Ode Okpe",
+        position:"Treasurer of Ofuk’Idoma Development Association",
+        portfolio:"He attended St. Barnard’s Primary School, Okpoga, Mt. St. Michael’s Secondary School, Aliade and University of Calabar, in Cross River State where he had his Bachelor of Arts and Master of Arts Degrees",
+        img:"https://firebasestorage.googleapis.com/v0/b/ofukidoma1-8a9d7.appspot.com/o/TEAM%2Foche.jpg?alt=media&token=095d1099-d8f0-462e-acf0-b8586c83cc47",
+        about:"having a successful career with the Nigeria Customs Service as a retired Comptroller of Customs, he developed some management and leadership skills especially the power of effective supervision and perfection in the performance of assigned duties. And he is happily married to Mrs Patricia Ene Okpe."
+    }
+]
