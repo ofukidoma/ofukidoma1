3250bca3cd224d760ed5488128317e5c21e3bc1f
Joelodufu <joel.odufu@gmail.com>
2022-05-12 15:47:06 +0100

    added exco complete

--- public/assets/js/Excos.js
+++ public/assets/js/Excos.js
@@ -16,9 +16,11 @@ const Exco = () => {
         .get()
         .then((querySnapshot) => {
           querySnapshot.forEach((doc) => {
-            var member = doc.data();
-            member.id = doc.id;
-            excoMember.push(member);
+            var exco = doc.data();
+            exco.id = doc.id;
+            if (exco.id<16) {
+              excoMember.push(exco);
+            }
           });
           var sortedExcoMembers = excoMember.sort(function(a,b){
             return a.id-b.id;
@@ -27,111 +29,146 @@ const Exco = () => {
         });
     }, []);
   
-    var openProfile=(member)=>{
-      const myMember= JSON.stringify(member)
-      localStorage.setItem('member', myMember)
+    var openProfile=(exco)=>{
+      const myMember= JSON.stringify(exco)
+      localStorage.setItem('exco', myMember)
       window.location.assign('./profile2.html')
     }
   
-    return excoMember2.map((member) => (
-      <div className="col-12 col-md-6" key={member.id}>
-        <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
-          <div className="card-body p-4 conts">
-            <div className="member-profile position-absolute w-100 text-center">
-              <img
-                className="rounded-circle mx-auto d-inline-block shadow-sm"
-                src={member.img}
-                alt=""
-              />
-            </div>
-            <div className="card-text pt-1">
-              <h5 className="member-name mb-0 text-center text-primary font-weight-bold">
-                {member.name}
-              </h5>
-              <p className='justify-text'>{member.portfolio}</p>
-            </div>
-          </div>
+    return excoMember2.map((exco) => (
+
+      <div
+								className="u-align-center-lg u-align-center-md u-align-center-sm u-container-style u-list-item u-repeater-item">
+								<div className="u-container-layout u-similar-container u-valign-top u-container-layout-1">
+									<img className="u-expand-resize u-expanded-width-lg u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-image u-image-1"
+										src={exco.img}/>
+									<div
+										className="u-align-center-sm u-align-center-xs u-container-style u-group u-white u-group-1">
+										<div className="u-container-layout u-valign-top u-container-layout-2">
+											<h4 className="u-align-center u-custom-font u-font-oswald u-text u-text-4">{exco.name}</h4>
+											<h6
+												className="u-align-center u-custom-font u-font-pt-sans u-text u-text-palette-2-base u-text-5">{exco.position}</h6>
+											<p className="u-align-center u-text u-text-6">
+											</p>
+											<div className="u-social-icons u-spacing-10 u-social-icons-1">
+												<a className="u-social-url" target="_blank" href=""><span
+														className="u-icon u-icon-circle u-social-facebook u-social-icon u-icon-1"></span>
+												</a>
+												<a className="u-social-url" target="_blank" href=""><span
+														className="u-icon u-icon-circle u-social-icon u-social-twitter u-icon-2"></span>
+												</a>
+												<a className="u-social-url" target="_blank" href=""><span
+														className="u-icon u-icon-circle u-social-icon u-social-instagram u-icon-3"></span>
+												</a>
+											</div>
+										</div>
+									</div>
+								</div>
+							</div>
+      
+
+
+
+
+
+      // <div className="col-12 col-md-6" key={exco.id}>
+      //   <div className="card border-0 shadow-lg pt-5 my-5 position-relative">
+      //     <div className="card-body p-4 conts">
+      //       <div className="exco-profile position-absolute w-100 text-center">
+      //         <img
+      //           className="rounded-circle mx-auto d-inline-block shadow-sm"
+      //           src={exco.img}
+      //           alt=""
+      //         />
+      //       </div>
+      //       <div className="card-text pt-1">
+      //         <h5 className="exco-name mb-0 text-center text-primary font-weight-bold">
+      //           {exco.name}
+      //         </h5>
+      //         <p className='justify-text'>{exco.portfolio}</p>
+      //       </div>
+      //     </div>
           
   
-          <div className="card-footer theme-bg-primary border-0 text-center">
-            <ul className="social-list list-inline mb-0 mx-auto">
-              <li className="list-inline-item">
-                <a className="text-dark" href="#">
-                  <svg
-                    className="svg-inline--fa fa-linkedin-in fa-w-14 fa-fw"
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fab"
-                    data-icon="linkedin-in"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 448 512"
-                    data-fa-i2svg=""
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
-                    ></path>
-                  </svg>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a className="text-dark" href="#">
-                  <svg
-                    className="svg-inline--fa fa-twitter fa-w-16 fa-fw"
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fab"
-                    data-icon="twitter"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 512 512"
-                    data-fa-i2svg=""
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
-                    ></path>
-                  </svg>
-                </a>
-              </li>
-              <li className="list-inline-item">
-                <a className="text-dark" href="#">
-                  <svg
-                    className="svg-inline--fa fa-medium fa-w-14 fa-fw"
-                    aria-hidden="true"
-                    focusable="false"
-                    data-prefix="fab"
-                    data-icon="medium"
-                    role="img"
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 448 512"
-                    data-fa-i2svg=""
-                  >
-                    <path
-                      fill="currentColor"
-                      d="M0 32v448h448V32H0zm372.2 106.1l-24 23c-2.1 1.6-3.1 4.2-2.7 6.7v169.3c-.4 2.6.6 5.2 2.7 6.7l23.5 23v5.1h-118V367l24.3-23.6c2.4-2.4 2.4-3.1 2.4-6.7V199.8l-67.6 171.6h-9.1L125 199.8v115c-.7 4.8 1 9.7 4.4 13.2l31.6 38.3v5.1H71.2v-5.1l31.6-38.3c3.4-3.5 4.9-8.4 4.1-13.2v-133c.4-3.7-1-7.3-3.8-9.8L75 138.1V133h87.3l67.4 148L289 133.1h83.2v5z"
-                    ></path>
-                  </svg>
-                </a>
-              </li>
-            </ul>
+      //     <div className="card-footer theme-bg-primary border-0 text-center">
+      //       <ul className="social-list list-inline mb-0 mx-auto">
+      //         <li className="list-inline-item">
+      //           <a className="text-dark" href="#">
+      //             <svg
+      //               className="svg-inline--fa fa-linkedin-in fa-w-14 fa-fw"
+      //               aria-hidden="true"
+      //               focusable="false"
+      //               data-prefix="fab"
+      //               data-icon="linkedin-in"
+      //               role="img"
+      //               xmlns="http://www.w3.org/2000/svg"
+      //               viewBox="0 0 448 512"
+      //               data-fa-i2svg=""
+      //             >
+      //               <path
+      //                 fill="currentColor"
+      //                 d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
+      //               ></path>
+      //             </svg>
+      //           </a>
+      //         </li>
+      //         <li className="list-inline-item">
+      //           <a className="text-dark" href="#">
+      //             <svg
+      //               className="svg-inline--fa fa-twitter fa-w-16 fa-fw"
+      //               aria-hidden="true"
+      //               focusable="false"
+      //               data-prefix="fab"
+      //               data-icon="twitter"
+      //               role="img"
+      //               xmlns="http://www.w3.org/2000/svg"
+      //               viewBox="0 0 512 512"
+      //               data-fa-i2svg=""
+      //             >
+      //               <path
+      //                 fill="currentColor"
+      //                 d="M459.37 151.716c.325 4.548.325 9.097.325 13.645 0 138.72-105.583 298.558-298.558 298.558-59.452 0-114.68-17.219-161.137-47.106 8.447.974 16.568 1.299 25.34 1.299 49.055 0 94.213-16.568 130.274-44.832-46.132-.975-84.792-31.188-98.112-72.772 6.498.974 12.995 1.624 19.818 1.624 9.421 0 18.843-1.3 27.614-3.573-48.081-9.747-84.143-51.98-84.143-102.985v-1.299c13.969 7.797 30.214 12.67 47.431 13.319-28.264-18.843-46.781-51.005-46.781-87.391 0-19.492 5.197-37.36 14.294-52.954 51.655 63.675 129.3 105.258 216.365 109.807-1.624-7.797-2.599-15.918-2.599-24.04 0-57.828 46.782-104.934 104.934-104.934 30.213 0 57.502 12.67 76.67 33.137 23.715-4.548 46.456-13.32 66.599-25.34-7.798 24.366-24.366 44.833-46.132 57.827 21.117-2.273 41.584-8.122 60.426-16.243-14.292 20.791-32.161 39.308-52.628 54.253z"
+      //               ></path>
+      //             </svg>
+      //           </a>
+      //         </li>
+      //         <li className="list-inline-item">
+      //           <a className="text-dark" href="#">
+      //             <svg
+      //               className="svg-inline--fa fa-medium fa-w-14 fa-fw"
+      //               aria-hidden="true"
+      //               focusable="false"
+      //               data-prefix="fab"
+      //               data-icon="medium"
+      //               role="img"
+      //               xmlns="http://www.w3.org/2000/svg"
+      //               viewBox="0 0 448 512"
+      //               data-fa-i2svg=""
+      //             >
+      //               <path
+      //                 fill="currentColor"
+      //                 d="M0 32v448h448V32H0zm372.2 106.1l-24 23c-2.1 1.6-3.1 4.2-2.7 6.7v169.3c-.4 2.6.6 5.2 2.7 6.7l23.5 23v5.1h-118V367l24.3-23.6c2.4-2.4 2.4-3.1 2.4-6.7V199.8l-67.6 171.6h-9.1L125 199.8v115c-.7 4.8 1 9.7 4.4 13.2l31.6 38.3v5.1H71.2v-5.1l31.6-38.3c3.4-3.5 4.9-8.4 4.1-13.2v-133c.4-3.7-1-7.3-3.8-9.8L75 138.1V133h87.3l67.4 148L289 133.1h83.2v5z"
+      //               ></path>
+      //             </svg>
+      //           </a>
+      //         </li>
+      //       </ul>
             
-          </div>
-          <button
-            member={member}
-            onClick={()=>openProfile(member)}
-            type="button"
-            class="btn btn-primary"
-            data-toggle="modal"
-            data-target="#exampleModalLong"
-          >
-            View More
-          </button>
-        </div>
-      </div>
+      //     </div>
+      //     <button
+      //       exco={exco}
+      //       onClick={()=>openProfile(exco)}
+      //       type="button"
+      //       className="btn btn-primary"
+      //       data-toggle="modal"
+      //       data-target="#exampleModalLong"
+      //     >
+      //       View More
+      //     </button>
+      //   </div>
+      // </div>
     ));
   };
   
-  ReactDOM.render(<Exco />, document.getElementById("membership"));
+  ReactDOM.render(<Exco />, document.getElementById("excoses"));
   
